ea1c7ad1e6d891a544ebb256fce3e6309f29ae47
DeserveHaving <810229530@qq.com>
2016-05-09 14:13:05 +0800

    bug修复
    修复bug

--- CountView/CountView.swift
+++ CountView/CountView.swift
@@ -8,55 +8,81 @@
 
 import UIKit
 
+/// a Control for count add/plus
 class CounterView: UIView, UITextFieldDelegate {
     
     var minValue: Int = 1
-    var maxValue: Int = 999
-    private(set) var currentValue = 1
+    var maxValue: Int = 99
     
-    var hGap: CGFloat = 2
-    var vGap: CGFloat = 2
+    var valueChanged: ((value: Int) -> Void)?
     
-    
-    var countControlSideLength: CGFloat = 40 {
+    private(set) var currentValue = 1 {
+        
         didSet {
-            setNeedsLayout()
+            if currentValue <= minValue {
+                plusControl.enabled = false
+            }else if currentValue == maxValue {
+                addControl.enabled = false
+            }else {
+                plusControl.enabled = true
+                addControl.enabled = true
+            }
+            valueChanged?(value: currentValue)
         }
     }
     
-    var styleColor: UIColor = UIColor.blackColor() {
+    /// 边框颜色
+    var styleLineColor: UIColor = UIColor.blackColor() {
         didSet {
-            addControl.lineColor = styleColor
-            plusControl.lineColor = styleColor
-            setNeedsDisplay()
+            addControl.lineColor = styleLineColor
+            plusControl.lineColor = styleLineColor
         }
     }
+    
+    /// 风格颜色
+    var styleColer: UIColor = UIColor.blackColor() {
+        didSet {
+            addControl.plusColor = styleColer
+            plusControl.plusColor = styleColer
+            textfield.textColor = styleColer
+        }
+    }
+    
+    /// 边框宽度
+    var lineWidth: CGFloat = 1 {
+        didSet {
+            addControl.lineWidth = lineWidth
+            plusControl.lineWidth = lineWidth
+        }
+    }
+    /// 加减控件的加减线条宽度
+    var plusWidth: CGFloat = 2
+    
     
     var hightlightColor: UIColor = UIColor(white: 0.8, alpha: 1) {
         didSet {
             addControl.hightlightColor = hightlightColor
             plusControl.hightlightColor = hightlightColor
-            setNeedsDisplay()
         }
     }
     
-    var lineWidth: CGFloat = 1 {
-        didSet {
-            addControl.lineWidth = lineWidth
-            plusControl.lineWidth = lineWidth
-            setNeedsDisplay()
-        }
-    }
-    
+    /// 加号控件距离父视图的水平距离
+    private var hGap: CGFloat = 2
+    private var vGap: CGFloat = 2
+    /// textField距离加减控件的左右边距
+    private var filedGap: CGFloat = 5
+    /// 加减控件边长
+    private var countControlSideLength: CGFloat = 40
     
     private var addControl: CountControl!
     private var plusControl: CountControl!
     private var textfield: UITextField!
     
+    
+    //MARK: - Initerlizers
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = UIColor.whiteColor()
         let contidion = (frame.size.width >= 120 && frame.size.height >= 40)
         precondition(contidion, "frame too small")
         
@@ -65,22 +91,33 @@ class CounterView: UIView, UITextFieldDelegate {
     
     convenience init(frame: CGRect, maxValue: Int, minValue: Int) {
         self.init(frame: frame)
+        
+        let contidion = (frame.size.width >= 120 && frame.size.height >= 40)
+        precondition(contidion, "frame too small")
+        
         self.maxValue = maxValue
         self.minValue = minValue
         currentValue = minValue
     }
     
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    //MARK: - Privates
     private func configureSubViews() {
         
         plusControl = CountControl(type: .System)
-        plusControl.addTarget(self, action: #selector(CounterView.add(_:)), forControlEvents: .TouchUpInside)
-
+        plusControl.addTarget(self, action: #selector(CounterView.addOrPlus(_:)), forControlEvents: .TouchUpInside)
+        plusControl.enabled = false
+        
         addControl = CountControl(type: .System)
         addControl.isAddButton = true
-        addControl.addTarget(self, action: #selector(CounterView.add(_:)), forControlEvents: .TouchUpInside)
+        addControl.addTarget(self, action: #selector(CounterView.addOrPlus(_:)), forControlEvents: .TouchUpInside)
         
         textfield = UITextField()
         textfield.textAlignment = .Center
+        textfield.layer.borderWidth = 1
         textfield.keyboardType = .NumberPad
         textfield.delegate = self
         
@@ -89,17 +126,13 @@ class CounterView: UIView, UITextFieldDelegate {
         addSubview(textfield)
     }
     
-    func add(btn: UIButton)  {
+    func addOrPlus(btn: UIButton)  {
         if  btn == plusControl {
             currentValue = currentValue <= minValue ? minValue : currentValue - 1
         }else {
             currentValue = currentValue >= maxValue ? maxValue : currentValue + 1
         }
         textfield.text = String(currentValue)
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
     }
     
     override func layoutSubviews() {
@@ -115,31 +148,15 @@ class CounterView: UIView, UITextFieldDelegate {
                                   y: CGRectGetMinY(plusControl.frame),
                                   width: CGRectGetWidth(plusControl.frame),
                                   height: CGRectGetHeight(plusControl.frame))
-        let rect = CGRect(x: CGRectGetMaxX(plusControl.frame),
+        let rect = CGRect(x: CGRectGetMaxX(plusControl.frame) + filedGap,
                           y: CGRectGetMinY(plusControl.frame),
-                          width: CGRectGetWidth(bounds) - 2 * countControlSideLength - 2 * hGap,
+                          width: CGRectGetWidth(bounds) - 2 * countControlSideLength - 2 * hGap - 2 * filedGap,
                           height: countControlSideLength)
         textfield.frame = rect
         textfield.text = String(currentValue)
+        textfield.layer.borderColor = styleLineColor.CGColor
     }
 
-    
-    // Only override drawRect: if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func drawRect(rect: CGRect) {
-        // Drawing code
-        
-        styleColor.setStroke()
-        
-        let path = UIBezierPath()
-        path.lineWidth = lineWidth
-        path.moveToPoint(CGPoint(x: CGRectGetMinX(textfield.frame), y: CGRectGetMinY(plusControl.frame)+1))
-        path.addLineToPoint(CGPoint(x: CGRectGetMaxX(textfield.frame), y: CGRectGetMinY(plusControl.frame)+1))
-        path.moveToPoint(CGPoint(x: CGRectGetMinX(textfield.frame), y: CGRectGetMaxY(plusControl.frame)-1))
-        path.addLineToPoint(CGPoint(x: CGRectGetMaxX(textfield.frame), y: CGRectGetMaxY(plusControl.frame)-1))
-        path.stroke()
-    }
- 
     // MARK: - UITextFieldDelegate
     func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
         
@@ -180,19 +197,29 @@ class CounterView: UIView, UITextFieldDelegate {
     }
     
     func textFieldDidEndEditing(textField: UITextField) {
-        if textfield.text?.lengthOfBytesUsingEncoding(NSUTF8StringEncoding) > 0  {
+        if textfield.text?.isEmpty == false {
             currentValue = Int(textField.text!)!
+        }else {
+            textField.text = String(currentValue)
         }
     }
 }
 
 @IBDesignable
+/// a control can plus or add
 class CountControl: UIButton {
     
+    /// 线框颜色
     @IBInspectable var lineColor: UIColor = UIColor.redColor()
+    /// 加减按钮颜色
+    @IBInspectable var plusColor: UIColor = UIColor.blackColor()
     @IBInspectable private var fillColor: UIColor = UIColor.clearColor()
-    @IBInspectable var lineWidth: CGFloat = 1
+    /// 线框宽度
+    @IBInspectable var lineWidth: CGFloat = 2
+    @IBInspectable var plusWidth: CGFloat = 2
+    /// 是否是加号控件，否则是减号控件
     @IBInspectable var isAddButton: Bool = false
+    /// 点击后的高亮颜色
     var hightlightColor: UIColor = UIColor(white: 0.8, alpha: 1)
     
     override func drawRect(rect: CGRect) {
@@ -202,27 +229,29 @@ class CountControl: UIButton {
         let path = UIBezierPath(rect: bounds)
         path.lineWidth = lineWidth
         path.stroke()
+        path.closePath()
         
-        let plusHeight: CGFloat = 3.0
-        let plusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
-        path.lineWidth = plusHeight
+        let plusPath = UIBezierPath()
+//        let plusHeight: CGFloat = 3.0
+        let theplusWidth: CGFloat = min(bounds.width, bounds.height) * 0.6
+        plusPath.lineWidth = plusWidth
         
-        path.moveToPoint(CGPoint(
-            x:bounds.width/2 - plusWidth/2,
+        plusPath.moveToPoint(CGPoint(
+            x:bounds.width/2 - theplusWidth/2,
             y:bounds.height/2))
         
-        path.addLineToPoint(CGPoint(
-            x:bounds.width/2 + plusWidth/2,
+        plusPath.addLineToPoint(CGPoint(
+            x:bounds.width/2 + theplusWidth/2,
             y:bounds.height/2))
         
         if isAddButton {
-            path.moveToPoint(CGPoint(
+            plusPath.moveToPoint(CGPoint(
                 x:bounds.width/2 + 0.5,
-                y:bounds.height/2 - plusWidth/2 + 0.5))
+                y:bounds.height/2 - theplusWidth/2 + 0.5))
             
-            path.addLineToPoint(CGPoint(
+            plusPath.addLineToPoint(CGPoint(
                 x:bounds.width/2 + 0.5,
-                y:bounds.height/2 + plusWidth/2 + 0.5))
+                y:bounds.height/2 + theplusWidth/2 + 0.5))
         }
         
         let ctx = UIGraphicsGetCurrentContext()
@@ -232,8 +261,14 @@ class CountControl: UIButton {
             CGContextFillPath(ctx)
         }
 
-        path.stroke()
-        path.fill()
+        if enabled {
+            plusColor.setStroke()
+        }else {
+            UIColor.lightGrayColor().setStroke()
+        }
+        
+        plusPath.stroke()
+        path.appendPath(plusPath)
     }
     
     override func beginTrackingWithTouch(touch: UITouch, withEvent event: UIEvent?) -> Bool {
--- CountView/ViewController.swift
+++ CountView/ViewController.swift
@@ -13,11 +13,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let frame = CGRect(x: 100, y: 200, width: 120, height: 44)
-        let aview = CounterView(frame: frame, maxValue: 666, minValue: 2)
-        aview.styleColor = UIColor(red: 100/255.0, green: 200/255.0, blue: 100/255.0, alpha: 1)
-        aview.countControlSideLength = 30
-        aview.hightlightColor = UIColor.redColor()
+        let frame = CGRect(x: 100, y: 200, width: 140, height: 44)
+        let aview = CounterView(frame: frame, maxValue: 666, minValue: 1)
+        aview.styleLineColor = UIColor.lightGrayColor()
+//        aview.countControlSideLength = 30
+        aview.hightlightColor = UIColor.lightGrayColor()
+        aview.valueChanged = { d in
+            print("changed to value: \(d)")
+        }
         view.addSubview(aview)
     }
 
